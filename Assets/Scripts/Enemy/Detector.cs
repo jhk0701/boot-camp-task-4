832e22c9dc3a16b1e2de56622c01dab7a172484e
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2024-11-12 09:32:14 +0900

    Feat : Add detector

--- Assets/Scripts/Enemy/Detector.cs
+++ Assets/Scripts/Enemy/Detector.cs
@@ -1,8 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SphereCollider))]
 public class Detector : MonoBehaviour
 {
-    
+    SphereCollider _collider;
+    [SerializeField] Enemy enemy;
+    [SerializeField] LayerMask layerMask;
+
+
+    void Awake()
+    {
+        _collider = GetComponent<SphereCollider>();
+        _collider.radius = CharacterManager.Instance.enemyConfig.detectDistance;
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (IsMatched(layerMask.value, other.gameObject.layer))
+        {
+            if (other.gameObject.TryGetComponent(out Player player))
+                enemy.Target = player;
+        }
+    }
+
+    // 추적을 포기하는 건 enemy state에서 처리
+
+    bool IsMatched(int value, int layer)
+    {
+        return value == (1 << layer | value);
+    }
 }
